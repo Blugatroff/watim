835b6bdc5d4f75ba88df2021aa9c1fb9f3b7e615
Jasper Reef <jasper.reef@gmx.de>
2023-03-19 10:34:55 +0100

    dead code elimination

--- recompile-compiler.sh
+++ recompile-compiler.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 set -xe
-./run.sh ./native/main.watim ./native/main.watim > watim.wat
+wasmtime --dir=. ./watim.wasm ./native/main.watim > out.wat && wasmtime --dir=. ./out.wat -- ./native/main.watim > watim.wat
 wat2wasm ./watim.wat -o ./watim.wasm
 rm ./watim.wat
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-wasmtime --dir=. ./watim.wasm $1 > out.wat && wasmtime --dir=. ./out.wat -- "${@:2}"
+wasmtime --dir=. ./watim.wasm $1 -- -q > out.wat && wasmtime --dir=. ./out.wat -- "${@:2}"
