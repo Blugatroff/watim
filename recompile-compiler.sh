af3828bb87e8e016fdbd9b8417b89e7243096e52
Jasper Reef <jasper.reef@gmx.de>
2023-05-01 11:28:00 +0200

    Array initialization

--- recompile-compiler.sh
+++ recompile-compiler.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 set -xe
+
 wasmtime --dir=. ./watim.wasm ./native/main.watim > out.wat && wasmtime --dir=. ./out.wat -- ./native/main.watim > watim.wat
 wat2wasm ./watim.wat -o ./watim.wasm
 rm ./watim.wat
+
