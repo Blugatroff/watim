ed25ee5e324bc72bc449330f05999f21bc1ff55c
Jasper Reef <mail@jasperreef.com>
2024-08-09 12:28:31 +0200

    test expectation update command `./test.py write ..`

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import json
 import pathlib
 import sys
 import os
+import difflib
 
 from termcolor import colored
 
@@ -17,10 +18,17 @@ if not os.path.isfile("test.wat") and subprocess.run(f"python bootstrap.py ./tes
     exit(1)
 
 def parse_test_file(path: str):
-    output = subprocess.run(f"wasmtime --dir=. -- test.wat {path}", shell=True, stdout=subprocess.PIPE)
+    output = subprocess.run(f"wasmtime --dir=. -- test.wat read {path}", shell=True, stdout=subprocess.PIPE)
     if output.returncode != 0:
         return None
     return json.loads(output.stdout)
+
+def write_test_file(path: str, test: dict):
+    print(json.dumps(test))
+    output = subprocess.run(["wasmtime", "--dir=.", "--", "./test.wat", "write", path], input=bytes(json.dumps(test), 'UTF-8'))
+    if output.returncode != 0:
+        print(output, file=sys.stderr)
+        return exit(1)
 
 @dataclass
 class CompilerOutput:
@@ -29,12 +37,15 @@ class CompilerOutput:
     stderr: str
 
 already_compiled = False
-def run_native_compiler(args: List[str] | None, stdin: str):
+def compile_native_compiler():
     global already_compiled
     if not already_compiled:
         already_compiled = True
         if subprocess.run(f"python bootstrap.py ./v2/main.watim > watim.wat", shell=True).returncode != 0:
             exit(1)
+
+def run_native_compiler(args: List[str] | None, stdin: str):
+    compile_native_compiler()
     compiler = subprocess.run(["wasmtime", "--dir=.", "--", "./watim.wat"] + (args or ["-"]), input=bytes(test["compiler-stdin"], 'UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return CompilerOutput(compiler.returncode, compiler.stdout.decode("UTF-8").strip(), compiler.stderr.decode("UTF-8").strip())
 
@@ -51,6 +62,36 @@ def run_bootstrap_compiler(args: List[str] | None, stdin: str):
     except Exception as e:
         return CompilerOutput(1, "", str(e))
 
+if len(sys.argv) > 2 and sys.argv[1] == "write":
+    path = sys.argv[2]
+    test = parse_test_file(path)
+    if "--native" in sys.argv:
+        compiler = run_native_compiler(test['compiler-args'], test['compiler-stdin'])
+    else:
+        compiler = run_bootstrap_compiler(test['compiler-args'], test['compiler-stdin'])
+    stdout = None
+    stderr = None
+    status = None
+    if compiler.returncode == 0 and test['status'] is not None:
+        with open('./out.wat', 'wb') as outwat:
+            outwat.write(compiler.stdout.encode("UTF-8"))
+        program = subprocess.run(["wasmtime", "./out.wat"], input=bytes(test["stdin"] or "", 'UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout = None if test['stdout'] is None else program.stdout
+        stderr = None if test['stderr'] is None else program.stderr
+        status = None if test['status'] is None else program.returncode
+    write_test_file(path, {
+        "compiler-stdin": test['compiler-stdin'],
+        "compiler-args": test['compiler-args'],
+        "compiler-stdout": None if test['compiler-stdout'] is None else compiler.stdout,
+        "compiler-stderr": None if test['compiler-stderr'] is None else compiler.stderr,
+        "compiler-status": None if test['compiler-status'] is None else compiler.returncode,
+        "stdin": test['stdin'],
+        "stdout": stdout,
+        "stderr": stderr,
+        "status": status,
+    })
+    exit(0)
+
 if len(sys.argv) > 2 and sys.argv[1] == "--native":
     pattern = sys.argv[2]
 elif len(sys.argv) == 2 and sys.argv[1] == "--native":
@@ -60,6 +101,22 @@ elif len(sys.argv) > 1:
 else:
     pattern = "./tests/*.watim"
 tests = glob.glob(pattern)
+
+def print_mismatch(expected: str, actual: str):
+    for line in difflib.unified_diff(expected.splitlines(), actual.splitlines(), fromfile='expected', tofile='actual'):
+        print(line, file=sys.stderr)
+
+@dataclass
+class Test:
+    compiler_stdin: str | None
+    compiler_status: int | None
+    compiler_stdout: str | None
+    compiler_stderr: str | None
+    stdin: str | None
+    status: int | None
+    stdout: str | None
+    stderr: str | None
+
 failed = False
 for path in tests:
     test = parse_test_file(path)
@@ -83,13 +140,11 @@ for path in tests:
         continue
     if test['compiler-stderr'] is not None and compiler.stderr != test['compiler-stderr'].strip():
         print(f"{path}: expected different compiler stderr:", file=sys.stderr)
-        print(f"Expected:\n{test['compiler-stderr']}", file=sys.stderr)
-        print(f"Actual:\n{compiler.stderr}", file=sys.stderr)
+        print_mismatch(test['compiler-stderr'], compiler.stderr)
         continue
     if test['compiler-stdout'] is not None and compiler.stdout != test['compiler-stdout'].strip():
         print(f"{path}: expected different compiler stdout:", file=sys.stderr)
-        print(f"Expected:\n{test['compiler-stdout']}", file=sys.stderr)
-        print(f"Actual:\n{compiler.stdout}", file=sys.stderr)
+        print_mismatch(test['compiler-stdout'], compiler.stdout)
         print(f"stderr was: {compiler.stderr}", file=sys.stderr)
         continue
     with open('./out.wat', 'wb') as outwat:
